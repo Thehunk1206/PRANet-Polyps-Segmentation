fixed the image data normalization problem

train_pranet.py
@@ -152,7 +152,7 @@ def train(
             tf.print(
                 f"Saving model at {trained_model_dir}..."
             )
-            praresnet.save(trained_model_dir + "pranet_v1", save_format='tf')
+            praresnet.save(trained_model_dir + "pranet_v1.1", save_format='tf')
             tf.print(f"model saved at {trained_model_dir}")
         
 

utils/dataset.py
@@ -84,11 +84,9 @@ class TfdataPipeline:
         img = tf.io.decode_jpeg(img_raw)
         mask = tf.io.decode_jpeg(mask_raw, channels=1)
 
-        img = tf.image.convert_image_dtype(img, tf.float32)
-        img = img/255.0
+        img = tf.image.convert_image_dtype(img, tf.float32) # normalize the values between 0-1
 
-        mask = tf.image.convert_image_dtype(mask, tf.float32)
-        mask = mask/255.0
+        mask = tf.image.convert_image_dtype(mask, tf.float32) # normalize the values between 0-1
 
         img = tf.image.resize(img, [self.IMG_H, self.IMG_W])
         mask = tf.image.resize(mask, [self.IMG_H, self.IMG_W])